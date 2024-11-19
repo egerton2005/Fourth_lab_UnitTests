4624d7d64ae93905f27aa22d38abda83a0a784f8
Egor Yanchukov <60086857+egerton2005@users.noreply.github.com>
2024-11-20 00:58:15 +0300

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -11,22 +11,12 @@ int Prime(int x){
     return 1;
 }
 
-bool Check(std::string prom){
-    int check = true;
-    for (int i = 0; i < prom.length(); i++) {
-        if('0' > (char)prom[i] && (char)prom[i] > '9') {
-            check = false;
-            break;
-        }
-    }
-
-    return check;
-}
-
 int main() {
-    std::string input;
-    std::cin >> input;
-    if(!Check(input)) std::cout<<0;
-    else std::cout << Prime(std::stoi(input)) << std::endl;
+    int input;
+    if(!(std::cin >> input)) {
+        std::cout<<0 << std::endl;
+        return 0;
+    }
+    std::cout << (Prime(n) ? 1 : 0) << std::endl;
     return 0;
 }
